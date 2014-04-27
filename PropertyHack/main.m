c7c980c2c0777a14953710e821c07e656e099313
Grayson <info@fromconcentratesoftware.com>
2014-04-26 19:07:21 -0700

    Initial Commit

---
+++ PropertyHack/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PropertyHack
+//
+//  Created by Grayson Hansard on 4/26/14.
+//  Copyright (c) 2014 From Concentrate Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
